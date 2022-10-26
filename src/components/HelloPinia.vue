38127aad03bde9365c2181ad0c5944f232eb1d8b
kanoi-y <furafuratonbo@gmail.com>
2022-10-26 21:51:55 +0900

    ✨ HelloPiniaを作成

---
+++ src/components/HelloPinia.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useStoreCounter } from '../stores/counter';
+const counter = useStoreCounter();
+
+const patch = () => {
+  counter.$patch({
+    count: 100,
+    user: {
+      name: 'Jane Doe',
+    },
+  });
+};
+</script>
+
+<template>
+  <h2>Hello Pinia</h2>
+  <p>カウント:{{ counter.count }}</p>
+  <button @click="patch">Patch</button>
+</template>
